7bbd98c00e0336a6cbba5126c47c4cd5227279c2
Ashish Garg <mragarg@gmail.com>
2019-05-09 10:26:15 -0400

    Generate ID function

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -2,16 +2,25 @@ import {
     ACTION_CREATE_POST
 } from '../actions';
 
+import { generateId } from '../utils';
+
 // A reducer is a function 
 // that accepts the current state
 // and an action 
 // then calculates/returns the next
 // new version of state
-export default function posts(state={}, action={type}) {
+export default function posts(state={}, action={type: ''}) {
     switch(action.type){
         case ACTION_CREATE_POST:
             // create a post!
-            console.log(`Sounds like you wanna create a post`);
+            const id = generateId();
+            console.log(`the new id is ${id}`);
+            // console.log(`Sounds like you wanna create a post`);
+            const newState = {
+                ...state,
+                // 1003: action.payload
+            }
+            return newState;
             break;
         default:
             return state;
