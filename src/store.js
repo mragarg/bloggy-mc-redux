dbb35c85be57df9b5e94ad3c573a0c7c714c8c3c
Ashish Garg <mragarg@gmail.com>
2019-05-09 11:23:55 -0400

    moves autosave to store.js

--- src/store.js
+++ src/store.js
@@ -5,6 +5,10 @@ import {
 
 import posts from './reducers/posts';
 
+const FREQUENCY = 2000;
+const LS_KEY = 'bloddy-mc-redux';
+
+
 // Pass an object to combineReducers.
 // This object should be 'shaped' like your state.
 const rootReducer = combineReducers({
@@ -15,5 +19,10 @@ const store = createStore(
     rootReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
+
+let saveInterval = setInterval(() => {
+    const state = store.getState();
+    localStorage.setItem(LS_KEY, JSON.stringify(state));
+}, FREQUENCY);
 
 export default store;
